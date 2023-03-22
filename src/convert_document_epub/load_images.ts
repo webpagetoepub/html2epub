a2dbd07d8390205cf8963ebb0e3a4e5d7a9f3ece
Carlson Santana Cruz <carlsonsantana@gmail.com>
2023-03-22 00:17:06 -0300

    Save repetead images once

--- src/convert_document_epub/load_images.ts
+++ src/convert_document_epub/load_images.ts
@@ -1,44 +1,55 @@
+import md5 from 'crypto-js/md5';
+
 import { loadFileFrom } from '../load_url';
 
 export default async function loadImages(element: Element, url: string) {
   const images = getAllImages(element);
+  replaceImagesSrcByAbsoluteSrc(images, url);
+  const imagesSrcs = getAllImagesSrcs(images);
+  replaceImagesByID(images);
 
-  const promises = images.map(
-    image => loadImage(image, url, replaceImageForID(image))
-  );
+  const promises = imagesSrcs.map(loadImage);
 
   return Promise.all(promises);
 }
 
 function getAllImages(element: Element) {
-  return Array.from(element.querySelectorAll('img[src]'));
+  return Array.from(
+    element.querySelectorAll('img[src]')
+  ).filter(element => element.getAttribute('src').trim());
 }
 
-async function loadImage(
-  image: Element,
-  pageURL: string,
-  callback: (blob: Blob) => {id: string, blob: Blob},
-) {
-  const imageSrc = image.getAttribute('src').trim();
+function replaceImagesSrcByAbsoluteSrc(images: Element[], pageURL: string) {
+  for (const image of images) {
+    const imageSrc = image.getAttribute('src').trim();
+    const imageAbsoluteSrc = new URL(imageSrc, pageURL).toString();
 
-  if (!imageSrc) {
-    return;
+    image.setAttribute('src', imageAbsoluteSrc);
+  }
+}
+
+function getAllImagesSrcs(images: Element[]) {
+  const imagesUrls: string[] = [];
+
+  for (const image of images) {
+    imagesUrls.push(image.getAttribute('src'));
   }
 
-  const imageUrl = new URL(imageSrc, pageURL).toString();
-
-  return await loadFileFrom(imageUrl).then(callback);
+  return Array.from(new Set(imagesUrls));
 }
 
-function replaceImageForID(
-  image: Element,
-): (blob: Blob) => {id: string, blob: Blob} {
-  return (blob: Blob) => {
-    const id = crypto.randomUUID();
+function replaceImagesByID(images: Element[]) {
+  for (const image of images) {
+    const imageSrc = image.getAttribute('src');
+    const id = md5(imageSrc);
 
     const comment = document.createComment(`<%= image['${id}'] %>`);
     image.parentNode.replaceChild(comment, image)
+  }
+}
 
-    return {id, blob};
-  };
+function loadImage(imageSrc: string) {
+  const id = md5(imageSrc);
+
+  return loadFileFrom(imageSrc).then(blob => ({id, blob}));
 }
