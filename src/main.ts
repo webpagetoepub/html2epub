f68e10a21612bf9e34a1949e2d144872ad5cc334
Carlson Santana Cruz <carlsonsantana@gmail.com>
2023-03-26 18:48:50 -0300

    Restore page elements from main module

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,47 @@ function convertPageToEPUB(url: string) {
     externalDOM => convertDocumentToEPUB(externalDOM, url)
   );
 }
+
+const formElement = document.getElementById('form');
+
+formElement.onsubmit = (event: Event) => {
+  event.preventDefault();
+  disableButton();
+
+  const inputUrlElement = document.getElementById('url') as HTMLInputElement;
+  const url = inputUrlElement.value;
+
+  convertPageToEPUB(url);
+
+  return false;
+};
+
+function disableButton() {
+  const element = document.querySelector(
+    'button[type=submit]'
+  ) as HTMLInputElement;
+
+  element.disabled = true;
+}
+
+function downloadEPUB(blob: Blob) {
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.textContent = 'Download EPUB';
+  link.download = 'pagina.epub';
+
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+
+  enableButton();
+}
+
+function enableButton() {
+  const element = document.querySelector(
+    'button[type=submit]'
+  ) as HTMLInputElement;
+
+  element.disabled = false;
+}
