4f0eb278759a5b33ac5b845c87ed933c1502118f
Carlson Santana Cruz <carlsonsantana@gmail.com>
2023-04-12 23:25:25 -0300

    Returns title with ePUB to allow set custom filename

--- src/convert_document_epub/create_epub.ts
+++ src/convert_document_epub/create_epub.ts
@@ -15,7 +15,7 @@ async function createEPUB(
   contents: SplittedContent[],
   metadata: any,
   images: {id: string, blob: Blob}[],
-): Promise<Blob> {
+): Promise<{title: string, epub: Blob}> {
   const jepub = new jEpub();
 
   jepub.init({ i18n: 'en', ...metadata });
@@ -28,13 +28,15 @@ async function createEPUB(
     jepub.add(content.title, content.content);
   }
 
-  return await jepub.generate('blob', (metadata: any) => {
+  const epub = await jepub.generate('blob', (metadata: any) => {
     console.log('progression: ' + metadata.percent.toFixed(2) + ' %');
 
     if (metadata.currentFile) {
       console.log('current file = ' + metadata.currentFile);
     }
   });
+
+  return {title: metadata.title, epub};
 }
 
 export default new Step(DESCRIPTION, createEPUB);
