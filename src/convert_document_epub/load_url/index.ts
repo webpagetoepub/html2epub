ef29ae57133c9539b4d26f80f8b8bea8860c16f4
Carlson Santana Cruz <carlsonsantana@gmail.com>
2024-05-11 13:19:56 -0300

    Request URLs to random proxies every request

--- src/convert_document_epub/load_url/index.ts
+++ src/convert_document_epub/load_url/index.ts
@@ -4,20 +4,40 @@ import directClient from './direct';
 import mangaraikuClient from './proxy_mangaraiku';
 import codetabsClient from './proxy_codetabs';
 import alloriginsClient from './proxy_allorigins';
+import Client from './client';
 
+
+const PROXY_CLIENTS = [mangaraikuClient, codetabsClient, alloriginsClient];
 
 export function requestTextContent(urlDescription: string) {
   return new Step(`Downloading "${urlDescription}"`, (url: string) => {
-    return directClient.requestTextContent(url)
-        .catch(() => mangaraikuClient.requestTextContent(url))
-        .catch(() => codetabsClient.requestTextContent(url))
-        .catch(() => alloriginsClient.requestTextContent(url));
+    return request(client => client.requestTextContent(url));
   });
 }
 
 export function loadFileFrom(url: string) {
-  return directClient.loadFileFrom(url)
-      .catch(() => mangaraikuClient.loadFileFrom(url))
-      .catch(() => codetabsClient.loadFileFrom(url))
-      .catch(() => alloriginsClient.loadFileFrom(url));
+  return request(client => client.loadFileFrom(url));
+}
+
+function request<T>(execute: (client: Client) => Promise<T>) {
+  const clients = sortRandomProxyClients();
+  let lastPromise = execute(directClient);
+
+  for (const client of clients) {
+    lastPromise = lastPromise.catch(() => execute(client));
+  }
+
+  return lastPromise;
+}
+
+function sortRandomProxyClients() {
+  const newProxyClients = PROXY_CLIENTS.concat([]);
+
+  return newProxyClients.sort(function() {
+    if (Math.random() > Math.random()) {
+      return 1;
+    }
+
+    return -1;
+  });
 }
